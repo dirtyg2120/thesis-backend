bba02d2bb95ceeb6b7a7f6d15db8f8e21a6d59d9
Phạm Hoàng Hải <hai.pham.hoang@hcmut.edu.vn>
2022-01-05 22:19:56 +0700

    refactor: remove unneede function in main.py

--- app/main.py
+++ app/main.py
@@ -4,16 +4,9 @@ from fastapi import FastAPI
 from .api import api_router
 from .core.config import settings
 
+app = FastAPI(title=settings.PROJECT_NAME, version=settings.VERSION)
 
-def get_application() -> FastAPI:
-    # application = FastAPI(title=PROJECT_NAME, debug=DEBUG, version=VERSION)
-    application = FastAPI(title=settings.PROJECT_NAME, version=settings.VERSION)
-    application.include_router(api_router, prefix=settings.API_PREFIX)
-
-    return application
-
-
-app = get_application()
+app.include_router(api_router, prefix=settings.API_PREFIX)
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=8080, reload=True, debug=False)
