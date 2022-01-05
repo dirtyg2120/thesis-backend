0d348c79d2726740903fdcddb8ad8ebdf9bb9358
Phạm Hoàng Hải <hai.pham.hoang@hcmut.edu.vn>
2022-01-05 20:49:48 +0700

    feat: use pydantic settings

--- app/core/config.py
+++ app/core/config.py
@@ -1,15 +1,17 @@
+from pydantic import BaseSettings
 
-import yaml
 
-config_file = 'config.yml'
-with open(config_file, "r") as f:
-    config = yaml.load(f, Loader=yaml.FullLoader)
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Bot Detector"
+    VERSION: str = "0.1.0"
+    API_PREFIX: str = "/api"
 
-PROJECT_NAME = config['project_name']
-VERSION = config['version']
-API_PREFIX = '/api'
+    CONSUMER_KEY: str
+    CONSUMER_SECRET: str
+    CALLBACK_URI: str = "oob"
 
-scraper_config = config['scraper_info']
-CONSUMER_KEY = scraper_config['consumer_key']
-CONSUMER_SECRET = scraper_config['consumer_secret']
-CALLBACK_URI = scraper_config['callback_uri']
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
--- app/services/scrape.py
+++ app/services/scrape.py
@@ -1,53 +1,64 @@
-import tweepy
-import time
-import pandas as pd
 import numpy as np
-from core.config import CONSUMER_KEY, CONSUMER_SECRET, CALLBACK_URI
+import pandas as pd
+import tweepy
+from core.config import settings
 
-class UserInfoScraper():
+
+class UserInfoScraper:
     def __init__(self, url_input) -> None:
-        self.auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET, CALLBACK_URI)
+        self.auth = tweepy.OAuthHandler(
+            settings.CONSUMER_KEY, settings.CONSUMER_SECRET, settings.CALLBACK_URI
+        )
         self.api = tweepy.API(self.auth, wait_on_rate_limit=True)
         self.user_url = url_input
-        self.user_name = self.user_url.split('/')[3]
+        self.user_name = self.user_url.split("/")[3]
 
     def get_followers(self, followers_numbs):
         followers = []
-        for follower in tweepy.Cursor(self.api.followers_ids, id=self.user_name).items(followers_numbs):
+        for follower in tweepy.Cursor(self.api.followers_ids, id=self.user_name).items(
+            followers_numbs
+        ):
             followers.append(follower)
 
         # followers_df = pd.DataFrame(followers)
         return followers
-    
+
     def get_followings(self, followings_numbs):
         followings = []
-        for following in tweepy.Cursor(self.api.friends_ids, id=self.user_name).items(followings_numbs):
+        for following in tweepy.Cursor(self.api.friends_ids, id=self.user_name).items(
+            followings_numbs
+        ):
             followings.append(following)
-        
+
         # followings_df = pd.DataFrame(followings)
         return followings
 
     def get_profile_info(self):
         user_api = self.api.get_user(screen_name=self.user_name)
-        columns = ['id', 
-           'name', 
-           'screen_name',
-           'created_at', 
-           'followers_count', 
-           'friends_count',
-           'profile_background_image_url',
-           'profile_image_url',
-           ]
-        
+        columns = [
+            "id",
+            "name",
+            "screen_name",
+            "created_at",
+            "followers_count",
+            "friends_count",
+            "profile_background_image_url",
+            "profile_image_url",
+        ]
+
         profile_info = []
         for attribute in columns:
             profile_info.append(getattr(user_api, attribute))
 
-        profile_info_df = pd.DataFrame(np.array(profile_info).reshape(1,len(columns)), columns=columns)
+        profile_info_df = pd.DataFrame(
+            np.array(profile_info).reshape(1, len(columns)), columns=columns
+        )
         return profile_info_df
 
     def get_tweets(self, tweets_numbs):
         tweets = []
-        for status in tweepy.Cursor(self.api.user_timeline, id=self.user_name, exclude_replies=False).items(tweets_numbs):
+        for status in tweepy.Cursor(
+            self.api.user_timeline, id=self.user_name, exclude_replies=False
+        ).items(tweets_numbs):
             tweets.append(status.text)
         return tweets
