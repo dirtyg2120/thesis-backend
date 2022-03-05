abee75359a7cfbd2729bccd6e0f8acbe8744a10b
Phạm Hoàng Hải <hai.pham.hoang@hcmut.edu.vn>
2022-03-05 08:14:23 +0700

    style: fix style

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -14,6 +14,7 @@ class DetailResponse(BaseModel):
     user_info: TwitterUser
     tweet_info: TweetInfo
 
+
 class AccountReport(BaseModel):
     # NOTE: this is just prototype, add more fields later!
     user_info: TwitterUser
