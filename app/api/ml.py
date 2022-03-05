8b0c6cfcde4c2f4023aa8eb52cbd31ede3200cd6
Phạm Hoàng Hải <hai.pham.hoang@hcmut.edu.vn>
2022-03-05 08:12:11 +0700

    style: fix module docstring

--- app/api/ml.py
+++ app/api/ml.py
@@ -1,3 +1,8 @@
+"""
+These are endpoints to handle
+- Adding user report as data for the ML service
+"""
+
 from fastapi import APIRouter, Depends, HTTPException, Response
 
 from app import schemas
@@ -7,15 +12,10 @@ router = APIRouter()
 user_auth_handler = UserAuthHandler()
 operator_auth_handler = OperatorAuthHandler()
 
-'''
-    These are endpoints to handle
-    - Adding user report as data for the ML service
-'''
-
 # Note: Operator only
-@router.get(
-    "/add-ml-data",name="operator:add-ml-data"
-)
-def add_ml_data(report_id: str, user_identifier=Depends(operator_auth_handler.auth_wrapper)):
+@router.get("/add-ml-data", name="operator:add-ml-data")
+def add_ml_data(
+    report_id: str, user_identifier=Depends(operator_auth_handler.auth_wrapper)
+):
     # TODO: Take report from db, scape needed data and add to ML's database somehow!
-    return 'success'
+    return "success"
--- app/api/report.py
+++ app/api/report.py
@@ -1,3 +1,9 @@
+"""
+These are endpoints to handle
+- User reporting wrong prediction result
+- Operator viewing reports
+"""
+
 from fastapi import APIRouter, Depends, HTTPException, Response
 
 from app import schemas
@@ -7,39 +13,32 @@ router = APIRouter()
 user_auth_handler = UserAuthHandler()
 operator_auth_handler = OperatorAuthHandler()
 
-'''
-    These are endpoints to handle
-    - User reporting wrong prediction result
-    - Operator viewing reports
-'''
-
 # Note: Operator only
 @router.get(
-    "/view-reports", response_model= schemas.AccountReport ,name="operator:view-report"
+    "/view-reports", response_model=schemas.AccountReport, name="operator:view-report"
 )
 def view_reports(user_identifier=Depends(operator_auth_handler.auth_wrapper)):
-    '''
-        TODO:
-        - Take report from DB
-        - Show needed information
-    '''
-    return [
-        { 'name' : 'report 1'},
-        { 'name' : 'report 2'}
-    ]
+    """
+    TODO:
+    - Take report from DB
+    - Show needed information
+    """
+    return [{"name": "report 1"}, {"name": "report 2"}]
 
 
 # NOTE: User only
 @router.post("/send-report", name="user:send-report")
-def send_report(account_id: str, user_identifier=Depends(user_auth_handler.auth_wrapper)):
-    '''
-        TODO:
-        1. If first report:
-            - Fetch account info from database or scrape
-            - Copy all needed into DB (Report table)
-            - Include a timestamp for the report
-            - Set report count as 1
-        2. Else:
-            - Increase report count
-    '''
-    return { 'status' : 'success', 'account' : 'twitter_account_1'}
+def send_report(
+    account_id: str, user_identifier=Depends(user_auth_handler.auth_wrapper)
+):
+    """
+    TODO:
+    1. If first report:
+        - Fetch account info from database or scrape
+        - Copy all needed into DB (Report table)
+        - Include a timestamp for the report
+        - Set report count as 1
+    2. Else:
+        - Increase report count
+    """
+    return {"status": "success", "account": "twitter_account_1"}
