d70ad25ca30f1f2f141f84b74f1940bc05dff77d
Phạm Hoàng Hải <hai.pham.hoang@hcmut.edu.vn>
2022-03-09 21:41:12 +0700

    refactor: move logger to global variable

--- app/services/clean_database.py
+++ app/services/clean_database.py
@@ -3,13 +3,14 @@ from datetime import datetime, timedelta
 
 from app.models import TwitterUser
 
+_logger = logging.getLogger(__name__)
+
 
 def clean_database(max_age: timedelta) -> None:
     """Clear analysis result older than max_age"""
-    logger = logging.getLogger(__name__)
     now = datetime.utcnow()
     try:
         count = TwitterUser.objects(timestamp__lt=now - max_age).delete()
-        logger.info(f"Clear {count} analysis results")
+        _logger.info(f"Clear {count} analysis results")
     except Exception as e:
-        logger.exception(e)
+        _logger.exception(e)
