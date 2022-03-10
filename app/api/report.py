78e9438430da97eccb231dbdf00760da00062f16
Phạm Hoàng Hải <hai.pham.hoang@hcmut.edu.vn>
2022-03-10 11:56:47 +0700

    feat: remove user_id parameter from send report endpoint
    There is no user_id, only session_id.
    And it is already in cookie.

--- app/api/report.py
+++ app/api/report.py
@@ -31,8 +31,8 @@ def view_reports(user_identifier=Depends(operator_auth_handler.auth_wrapper)):
 
 
 # NOTE: User only
-@router.post("/send-report/{user_id}", name="user:send-report")
-def send_report(user_id: str, user_identifier=Depends(user_auth_handler.auth_wrapper)):
+@router.post("/send-report", name="user:send-report")
+def send_report(user_identifier=Depends(user_auth_handler.auth_wrapper)):
     """
     TODO:
     1. If first report:
