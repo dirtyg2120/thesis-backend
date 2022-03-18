69e857931c8a4a7fc5178a733d4ff6a4484b589b
Phạm Hoàng Hải <hai.pham.hoang@hcmut.edu.vn>
2022-03-18 14:48:18 +0700

    feat: chaneg error message to match test

--- app/services/url_parser.py
+++ app/services/url_parser.py
@@ -8,7 +8,7 @@ def parse_twitter_url(url: str) -> Tuple[Literal["user", "tweet"], str]:
     parsed = urlparse(url)
 
     if parsed.netloc != "twitter.com":
-        raise HTTPException(400, "Invalid Twitter URL")
+        raise HTTPException(400, "'url' argument is invalid!")
 
     url_parts = parsed.path.split("/")[1:]
     if len(url_parts) >= 3 and url_parts[1] == "status":
@@ -18,4 +18,4 @@ def parse_twitter_url(url: str) -> Tuple[Literal["user", "tweet"], str]:
         username = url_parts[0]
         return "user", username
     else:
-        raise HTTPException(400, "Unknown Twitter URL")
+        raise HTTPException(400, "'url' argument is invalid!")
