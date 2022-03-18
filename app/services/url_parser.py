5374176e9bba3f4cf0f2ab6b7ce5f37691d564ce
Phạm Hoàng Hải <hai.pham.hoang@hcmut.edu.vn>
2022-03-18 14:45:33 +0700

    feat(parse_twitter_url): handle more edge cases

--- app/services/url_parser.py
+++ app/services/url_parser.py
@@ -1,17 +1,21 @@
 from typing import Literal, Tuple
 from urllib.parse import urlparse
 
+from fastapi import HTTPException
+
 
 def parse_twitter_url(url: str) -> Tuple[Literal["user", "tweet"], str]:
     parsed = urlparse(url)
 
     if parsed.netloc != "twitter.com":
-        raise ValueError("Invalid Twitter URL")
+        raise HTTPException(400, "Invalid Twitter URL")
 
     url_parts = parsed.path.split("/")[1:]
-    if url_parts[1] == "status":
+    if len(url_parts) >= 3 and url_parts[1] == "status":
         tweet_id = url_parts[2]
         return "tweet", tweet_id
-    else:
+    elif len(url_parts) >= 1:
         username = url_parts[0]
         return "user", username
+    else:
+        raise HTTPException(400, "Unknown Twitter URL")
