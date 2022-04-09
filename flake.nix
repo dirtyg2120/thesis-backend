6b70952d522d3e0ec9e0c0dfef5bd9ca7696e7b3
phhai-hcmut <66716510+phhai-hcmut@users.noreply.github.com>
2022-04-09 15:12:15 +0700

    Merge some changes from PR #40 (#47)
    * feat: add changes from PR #40
    
    * Update Nix development environment

--- flake.nix
+++ flake.nix
@@ -11,14 +11,54 @@
   };
 
   outputs = { self, nixpkgs, ... }@inputs:
-    nixpkgs.lib.flake.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+    nixpkgs.lib.flake.eachSystem [ "x86_64-linux" ] (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
       in
       {
         devShell = (pkgs.devshell.eval {
           configuration = ./devshell.nix;
           extraSpecialArgs = { inherit inputs; };
         }).shell;
+        checks =
+          let
+            pythonEnv = inputs.mach-nix.lib.${system}.mkPython {
+              requirements = builtins.readFile ./requirements.txt + ''
+                setuptools
+                flake8
+                flake8-isort
+                flake8-black
+                mypy
+              '';
+            };
+          in
+          {
+            lint = pkgs.runCommandLocal "lint"
+              {
+                src = ./.;
+                nativeBuildInputs = [ pythonEnv ];
+              } ''
+              cd $src
+              flake8 --show-source --statistics --count
+              touch $out
+            '';
+            test = pkgs.runCommandLocal "test"
+              {
+                src = ./.;
+                nativeBuildInputs = [ pythonEnv ];
+              } ''
+              cd $src
+              pytest -p no:cacheprovider app/tests
+              touch $out
+            '';
+            type-check = pkgs.runCommandLocal "type-check"
+              {
+                src = ./.;
+                nativeBuildInputs = [ pythonEnv ];
+              } ''
+              cd $src
+              mypy --no-incremental --package=app
+              touch $out
+            '';
+          };
       });
 }
