e53d3abb6abe09baa4596cc4d5d8dfe387a20c39
Produde (Long) <72970246+produdez@users.noreply.github.com>
2022-04-12 20:07:36 +0700

    Rename TwitterUser to BotPrediction (#46)
    * [refactor] rename TwitterUser to BotPrediction
    
    * [refactor] Embed User as field, some other refactors
    - BotPrediction and Report now have User as EmbeddedDocument
    - BotPrediction schema is now UserInfo schema
    - rename db collection name
    - rename tweet.py to twitter.py to contain both tweet and user
    - fix broken tests, change db query to match new model structure

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -2,16 +2,15 @@ from pydantic import BaseModel
 
 from .auth import AccessToken, AuthDetails, TokenPayload, UserIdentifier
 from .report import ReportResponse
-from .tweet import TweetInfo, TweetResponse
-from .twitter_user import TwitterUser
+from .twitter import TweetInfo, TweetResponse, UserInfo
 
 
 class CheckResponse(BaseModel):
     score: float
-    user_info: TwitterUser
+    user_info: UserInfo
 
 
 class DetailResponse(BaseModel):
-    user_info: TwitterUser
+    user_info: UserInfo
     tweet_info: TweetInfo
     score: float
--- app/services/clean_database.py
+++ app/services/clean_database.py
@@ -1,7 +1,7 @@
 import logging
 from datetime import datetime, timedelta
 
-from app.models import TwitterUser
+from app.models import BotPrediction
 
 _logger = logging.getLogger(__name__)
 
@@ -10,7 +10,7 @@ def clean_database(max_age: timedelta) -> None:
     """Clear analysis result older than max_age"""
     now = datetime.utcnow()
     try:
-        count = TwitterUser.objects(timestamp__lt=now - max_age).delete()
+        count = BotPrediction.objects(timestamp__lt=now - max_age).delete()
         _logger.info(f"Clear {count} analysis results")
     except Exception as e:
         _logger.exception(e)
