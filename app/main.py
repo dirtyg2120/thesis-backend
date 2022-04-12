e1b18afc4f2f5aad6860d9ee27df710f842dc798
Phạm Hoàng Hải <hai.pham.hoang@hcmut.edu.vn>
2022-04-12 11:19:32 +0700

    feat(uvicorn): only live reload in development

--- app/main.py
+++ app/main.py
@@ -79,7 +79,7 @@ if __name__ == "__main__":
         "app.main:app",
         host="0.0.0.0",
         port=8000,
-        reload=True,
+        reload=settings.PYTHON_ENV == "development",
         debug=False,
         log_config=log_config,
     )
