27e14d6e7a908768db274f223b7a9f3bef03b9c5
Phạm Hoàng Hải <hai.pham.hoang@hcmut.edu.vn>
2022-05-07 12:59:51 +0700

    fix: fix report feature

--- app/api/scraper.py
+++ app/api/scraper.py
@@ -34,12 +34,11 @@ def user_detail_check(
 
     username = get_twitter_username(url)
     prediction = bot_checker.check_account(username)
+    user_info = scraper.get_user_by_username(username)
 
-    recent_tweets = scraper.get_tweet_info(
-        prediction.user.twitter_id, settings.TWEETS_NUMBER
-    )
+    recent_tweets = scraper.get_tweet_info(user_info.id, settings.TWEETS_NUMBER)
 
-    day_of_week, hour_of_day = scraper.get_frequency(prediction.user.twitter_id)
+    day_of_week, hour_of_day = scraper.get_frequency(user_info.id)
     tweet_info = schemas.TweetInfo(
         day_of_week=day_of_week,
         hour_of_day=hour_of_day,
@@ -47,7 +46,7 @@ def user_detail_check(
     )
 
     response = schemas.DetailResponse(
-        user_info=scraper.get_user_by_username(username),
+        user_info=user_info,
         tweet_info=tweet_info,
         score=prediction.score,
     )
